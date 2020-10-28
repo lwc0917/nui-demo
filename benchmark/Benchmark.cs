7bded4243e1d98b0821a88a830be1d956d1bcda4
Woochanlee <wc0917.lee@samsung.com>
2020-10-28 15:25:26 +0900

    [Benchmark] Fix indentation

--- benchmark/Benchmark.cs
+++ benchmark/Benchmark.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -128,6 +128,7 @@ class HelloWorldExample : NUIApplication
             }
         }
     }
+
     void CreateImageViews()
     {
         int actorCount = mRowsPerPage * mColumnsPerPage * mPageCount;
